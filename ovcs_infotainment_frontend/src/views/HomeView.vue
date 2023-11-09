5565a4c6c0e56e7c5db774493da93fda06e627fd
Marc Lainez <marc.lainez@gmail.com>
2023-11-09 11:27:11 +0100

    Remove default boilerplate and replace with tailwindcss powered home

--- ovcs_infotainment_frontend/src/views/HomeView.vue
+++ ovcs_infotainment_frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-3xl font-bold underline">
+	OVCS
+  </h1>
 </template>
