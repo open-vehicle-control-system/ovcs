d1e5234684645e2299ccc0bfe1288fab7b735faf
Marc Lainez <marc.lainez@gmail.com>
2023-11-09 14:18:35 +0100

    Remove uneeded pages and add tailwindcss

--- ovcs_infotainment_frontend/src/App.vue
+++ ovcs_infotainment_frontend/src/App.vue
@@ -5,12 +5,16 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="bg-gray-800">
+	<div class="mx-auto px-2">
+	  <div class="relative flex h-16 items-center justify-between">
+      	    <div class="absolute inset-y-0 left-0 flex items-center">
+	      <RouterLink to="/" class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Home</RouterLink>
+	    </div>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
--- ovcs_infotainment_frontend/src/views/HomeView.vue
+++ ovcs_infotainment_frontend/src/views/HomeView.vue
@@ -2,7 +2,31 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-	OVCS
-  </h1>
+  <div class="container mx-auto">
+    <h2 class="text-4xl py-10">Status: {{content.status}}</h2>
+  </div>
 </template>
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+  data: () => ({
+    content: {status: "Loading ..."},
+  }),
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("http://localhost:4000/api/status");
+        this.content = await response.json();;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    setTimeout(() => this.getData(), 2000);
+  },
+
+};
+</script>
