c21c55f84520d737a25e29cf2337aab8196caf19
Marc Lainez <marc.lainez@gmail.com>
2023-11-09 23:07:47 +0100

    Use sidebar template

--- ovcs_infotainment_frontend/src/App.vue
+++ ovcs_infotainment_frontend/src/App.vue
@@ -1,20 +1,48 @@
+<template>
+  <div>
+    <!-- Static sidebar for desktop -->
+    <div class="lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-51 lg:flex-col">
+      <!-- Sidebar component, swap this element with another sidebar if you like -->
+      <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6">
+        <div class="flex h-16 shrink-0 items-center">
+          <p class="text-3xl text-white">OVCS</p>
+        </div>
+        <nav class="flex flex-1 flex-col">
+          <ul role="list" class="flex flex-1 flex-col gap-y-7">
+            <li>
+              <ul role="list" class="-mx-2 space-y-1">
+                <li v-for="item in navigation" :key="item.name">
+                  <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                    <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-20 w-20 shrink-0']" aria-hidden="true" />
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <main class="py-10 lg:pl-72">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
+
 <script setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import {
+  DocumentMagnifyingGlassIcon,
+  RadioIcon,
+} from '@heroicons/vue/24/outline'
+
+const navigation = [
+  { name: 'Debug', href: '/', icon: DocumentMagnifyingGlassIcon, current: true },
+]
+
+const sidebarOpen = ref(false)
 import { RouterLink, RouterView } from 'vue-router'
 </script>
-
-<template>
-  <header>
-    <div class="wrapper">
-      <nav class="bg-gray-800">
-	<div class="mx-auto px-2">
-	  <div class="relative flex h-16 items-center justify-between">
-      	    <div class="absolute inset-y-0 left-0 flex items-center">
-	      <RouterLink to="/" class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Home</RouterLink>
-	    </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </header>
-  <RouterView />
-</template>
