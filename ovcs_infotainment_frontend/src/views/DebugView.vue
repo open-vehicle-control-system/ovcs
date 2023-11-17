b2d87d2db13dd5fa5e43c41ef52a3195937d35ff
Marc Lainez <marc.lainez@gmail.com>
2023-11-17 10:07:30 +0100

    Add basic metrics to debugview

--- ovcs_infotainment_frontend/src/views/DebugView.vue
+++ ovcs_infotainment_frontend/src/views/DebugView.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { storeToRefs } from 'pinia'
+import { useMetricsStore } from "../stores/metrics.js"
+const { metrics } = storeToRefs(useMetricsStore())
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <h2 class="text-4xl py-10">Status: {{content.status}}</h2>
+  <div>
+    <div class="px-4 sm:px-0">
+      <h3 class="text-base font-semibold leading-7 text-gray-900">
+        Debug Information
+      </h3>
+    </div>
+    <div class="mt-6 border-t border-gray-100">
+      <dl class="divide-y divide-gray-100">
+        <template v-for="metric in metrics">
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt class="text-sm font-medium leading-6 text-gray-900">
+            {{ metric["attributes"]["name"] }}
+          </dt>
+          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {{ metric["attributes"]["value"] }} {{ metric["attributes"]["unit"]}}
+          </dd>
+        </div>
+        </template>
+      </dl>
+    </div>
   </div>
 </template>
 <script>
@@ -12,21 +33,8 @@ export default {
   components: {
   },
   data: () => ({
-    content: {status: "Loading ..."},
   }),
   methods: {
-    async getData() {
-      try {
-        let response = await fetch("http://localhost:4000/api/status");
-        this.content = await response.json();;
-      } catch (error) {
-        console.log(error);
-      }
-    },
   },
-  created() {
-    setTimeout(() => this.getData(), 2000);
-  },
-
 };
 </script>
