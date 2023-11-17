9d9b6b26d0e5578f39d9e0b03f81dfa3804a2129
Thibault Poncelet <thibault@spin42.com>
2023-11-17 14:32:00 +0100

    Return base structure on bootstrap

--- ovcs_infotainment_backend/lib/ovcs_infotainment_backend_web/sockets/dashboard/debug_metrics_channel.ex
+++ ovcs_infotainment_backend/lib/ovcs_infotainment_backend_web/sockets/dashboard/debug_metrics_channel.ex
@@ -7,7 +7,32 @@ defmodule OvcsInfotainmentBackendWeb.Sockets.Dashboard.DebugMetricsChannel do
   end
 
   def handle_in("bootstrap", _attrs, socket) do
-      response = %{sockay: "jason"}
-      {:reply, {:ok, response}, socket}
+      {:reply, {:ok, metrics()}, socket}
+  end
+
+
+  defp metrics do
+    %{
+      metrics: [
+        %{id: "1", type: "metric",  attributes: %{name: "speed", kind: "integer", unit: "m/s", origin: "abs", value: 14}},
+        %{id: "2", type: "metric",  attributes: %{name: "frontLeftWheelSpeed", kind: "integer", unit: "m/s", origin: "abs", value: 14}},
+        %{id: "3", type: "metric",  attributes: %{name: "frontRightWheelSpeed", kind: "integer", unit: "m/s", origin: "abs", value: 14}},
+        %{id: "4", type: "metric",  attributes: %{name: "rearLeftWheelSpeed", kind: "integer", unit: "m/s", origin: "abs", value: 14}},
+        %{id: "5", type: "metric",  attributes: %{name: "rearRightWheelSpeed", kind: "integer", unit: "m/s", origin: "abs", value: 14}},
+        %{id: "6", type: "metric",  attributes: %{name: "rotationsPerMinute", kind: "integer", unit: "rpm", origin: "engine", value: 1460}},
+        %{id: "7", type: "metric",  attributes: %{name: "coolingFuildTemp", kind: "integer", unit: "celcius", origin: "engine", value: 89}},
+        %{id: "8", type: "metric",  attributes: %{name: "incarAirbagSystemOnline", kind: "boolean", origin: "airbag", value: true}},
+        %{id: "9", type: "metric",  attributes: %{name: "passengerAirbagOnline", kind: "boolean", origin: "airbag", value: true}},
+        %{id: "10", type: "metric", attributes: %{name: "handbrakeEngaged", kind: "boolean", origin: "handbrake", value: false}},
+        %{id: "11", type: "metric", attributes: %{name: "steeringPumpActive", kind: "boolean", origin: "steering_pump", value: true}},
+        %{id: "12", type: "metric", attributes: %{name: "driverDoorOpen", kind: "boolean", origin: "doors", value: false}},
+        %{id: "13", type: "metric", attributes: %{name: "frontPassengerDoorOpen", kind: "boolean", origin: "doors", value: false}},
+        %{id: "14", type: "metric", attributes: %{name: "rearLeftPassengerDoorOpen", kind: "boolean", origin: "doors", value: false}},
+        %{id: "15", type: "metric", attributes: %{name: "rearRightPassengerDoorOpen", kind: "boolean", origin: "doors", value: false}},
+        %{id: "16", type: "metric", attributes: %{name: "trunkDoorOpen", kind: "boolean", origin: "doors", value: false}},
+        %{id: "17", type: "metric", attributes: %{name: "warningLightsActive", kind: "boolean", origin: "lights", value: false}},
+        %{id: "18", type: "metric", attributes: %{name: "beamActive", kind: "boolean", origin: "lights", value: true}}
+      ]
+    }
   end
 end
