72fe1be901ab4941a10f7acfd960dbeba99f4b83
Thibault Poncelet <thibault@spin42.com>
2023-11-23 13:54:16 +0100

    Store signals state and emit event only if something changed

--- ovcs_infotainment_backend/lib/ovcs_infotainment_backend/vehicle_state_manager.ex
+++ ovcs_infotainment_backend/lib/ovcs_infotainment_backend/vehicle_state_manager.ex
@@ -13,9 +13,11 @@ defmodule OvcsInfotainmentBackend.VehicleStateManager do
   def init([vehicle_config]) do
     {:ok,
       %{
-        "model" => vehicle_config["model"],
-        "brand" => vehicle_config["brand"],
-        "handbrakeEngaged" => initial_signal_state()
+        model: vehicle_config["model"],
+        brand: vehicle_config["brand"],
+        signals: %{
+          updated_at: nil
+        }
       }
     }
   end
@@ -24,13 +26,22 @@ defmodule OvcsInfotainmentBackend.VehicleStateManager do
   def handle_cast({:handle_frame, frame, signals}, state) do
     IO.inspect frame
     IO.inspect signals
-    signals |> Enum.each(fn(signal) ->
-      OvcsInfotainmentBackendWeb.Endpoint.broadcast!("debug-metrics", "update_handbrake", signal)
+    new_signals_state = signals |> Enum.reduce(state.signals, fn(signal, signals_state) ->
+      current_signal = signals_state[signal.name]
+      last_updated_at = signals_state.updated_at
+      new_state = case is_nil(current_signal) || current_signal.value != signal.value do
+        true ->
+          signals_state
+          |> Map.put(signal.name, signal)
+          |> Map.put(:updated_at, DateTime.to_unix(DateTime.utc_now()))
+        false ->
+          signals_state
+      end
+      if last_updated_at != new_state.updated_at do
+        OvcsInfotainmentBackendWeb.Endpoint.broadcast!("debug-metrics", "update_handbrake", signals |> List.first())
+      end
+      new_state
     end)
-    {:noreply, state}
-  end
-
-  defp initial_signal_state() do
-    %{updated: nil, value: nil}
+    {:noreply, %{state | signals: new_signals_state}}
   end
 end
