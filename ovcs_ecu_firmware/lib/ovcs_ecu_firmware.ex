4ed13be028836a9ad49cdd8aebbaca69d08ba8bf
Thibault Poncelet <thibault@spin42.com>
2024-01-19 11:00:58 +0100

    Setup ECU firmware

---
+++ ovcs_ecu_firmware/lib/ovcs_ecu_firmware.ex
@@ -0,0 +1,18 @@
+defmodule OvcsEcuFirmware do
+  @moduledoc """
+  Documentation for OvcsEcuFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OvcsEcuFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ovcs_ecu_firmware/lib/ovcs_ecu_firmware/application.ex
@@ -0,0 +1,44 @@
+defmodule OvcsEcuFirmware.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: OvcsEcuFirmware.Supervisor]
+
+    children =
+      [
+        # Children for all targets
+        # Starts a worker by calling: OvcsEcuFirmware.Worker.start_link(arg)
+        # {OvcsEcuFirmware.Worker, arg},
+      ] ++ children(target())
+
+    Supervisor.start_link(children, opts)
+  end
+
+  # List all child processes to be supervised
+  def children(:host) do
+    [
+      # Children that only run on the host
+      # Starts a worker by calling: OvcsEcuFirmware.Worker.start_link(arg)
+      # {OvcsEcuFirmware.Worker, arg},
+    ]
+  end
+
+  def children(_target) do
+    [
+      # Children for all targets except host
+      # Starts a worker by calling: OvcsEcuFirmware.Worker.start_link(arg)
+      # {OvcsEcuFirmware.Worker, arg},
+    ]
+  end
+
+  def target() do
+    Application.get_env(:ovcs_ecu_firmware, :target)
+  end
+end
