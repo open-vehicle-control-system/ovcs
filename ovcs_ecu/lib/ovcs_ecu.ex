efdedf0f15c380a9084c048394fa771e9c867374
Thibault Poncelet <thibault@spin42.com>
2024-01-22 15:13:34 +0100

    Add ECU app next to the ECU firmware

---
+++ ovcs_ecu/lib/ovcs_ecu.ex
@@ -0,0 +1,18 @@
+defmodule OvcsEcu do
+  @moduledoc """
+  Documentation for `OvcsEcu`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OvcsEcu.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ovcs_ecu/test/ovcs_ecu_test.exs
@@ -0,0 +1,8 @@
+defmodule OvcsEcuTest do
+  use ExUnit.Case
+  doctest OvcsEcu
+
+  test "greets the world" do
+    assert OvcsEcu.hello() == :world
+  end
+end
