5271e158c6ac3b8e8fd14908da8ed38f974eb084
Loïc Vigneron <loic.vigneron@gmail.com>
2024-02-12 14:03:26 +0100

    Finalize contactors controller

--- ovcs_controllers/ovcs_contactors_controller/src/main.cpp
+++ ovcs_controllers/ovcs_contactors_controller/src/main.cpp
@@ -1,66 +1,76 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
-struct can_frame receivedCanMessage;
-MCP2515 can_0(10);
-struct can_frame sentCanMessage;
-unsigned long sendingTimestamp = 0;
-unsigned long frequency = 10;
+#define CONTACTORS_STATUS_REQUEST_FRAME_ID 0x100
+#define CONTACTORS_STATUS_REQUEST_FRAME_LENGTH 3
+
+#define CONTACTORS_STATUS_FRAME_ID 0x101
+#define CONTACTORS_STATUS_FRAME_LENGTH 3
+
+#define CONTACTOR_STATUS_FRAME_FREQUENCY_MS 10
+
+#define MAIN_NEGATIVE_CONTACTOR_RELAY_PIN 8
+#define MAIN_POSITIVE_CONTACTOR_RELAY_PIN 4
+#define PRECHARGE_CONTACTOR_RELAY_PIN 7
+
+MCP2515 OVCS_CAN(10);
+
+unsigned long sendTimestamp = 0;
 unsigned long now = 0;
-struct can_frame contactorStateRequestFrame;
-struct can_frame contactorStatusFrame;
+
+struct can_frame contactorsStatusRequestFrame;
+struct can_frame contactorsStatusFrame;
+
 int mainNegativeContactorRequestedState = 0;
 int mainPositiveRequestedContactor = 0;
 int prechargeContactorRequestedState = 0;
 
-
-int main_negative_contactor_relay = 4;
-int main_positive_contactor_relay = 7;
-int precharge_contactor_relay = 8;
-
 void receive_frame() {
-  if (can_0.readMessage(&contactorStateRequestFrame) == MCP2515::ERROR_OK) {
-    Serial.print(contactorStateRequestFrame.can_id, HEX); // print ID
-    
-    for (int i = 0; i<contactorStateRequestFrame.can_dlc; i++)  {  // print the data
-      Serial.print(receivedCanMessage.data[i],HEX);
-      Serial.print(" ");
-    }
-    Serial.println();  
+  if (OVCS_CAN.readMessage(&contactorsStatusRequestFrame) == MCP2515::ERROR_OK) {
+    mainNegativeContactorRequestedState = contactorsStatusRequestFrame.data[0];
+    mainPositiveRequestedContactor = contactorsStatusRequestFrame.data[1];
+    prechargeContactorRequestedState = contactorsStatusRequestFrame.data[2];
   } 
 }
 
 void actuate_relays() {
-  //digitalWrite(relay_1, HIGH);
+  digitalWrite(MAIN_NEGATIVE_CONTACTOR_RELAY_PIN, mainNegativeContactorRequestedState == 1 ? HIGH : LOW);
+  digitalWrite(MAIN_POSITIVE_CONTACTOR_RELAY_PIN, mainPositiveRequestedContactor == 1 ? HIGH : LOW);
+  digitalWrite(PRECHARGE_CONTACTOR_RELAY_PIN, prechargeContactorRequestedState == 1 ? HIGH : LOW);
 }
 
 void send_status() {
   now = millis();
-  if (sendingTimestamp + frequency <= now) {
-    sendingTimestamp = now;
-    contactorStatusFrame.data[0] = 0;
-    contactorStatusFrame.data[1] = 0;
-    contactorStatusFrame.data[2] = 0;
-    can_0.sendMessage(&contactorStatusFrame);
+  if (sendTimestamp + CONTACTOR_STATUS_FRAME_FREQUENCY_MS <= now) {
+    sendTimestamp = now;
+    contactorsStatusFrame.data[0] = digitalRead(MAIN_NEGATIVE_CONTACTOR_RELAY_PIN);
+    contactorsStatusFrame.data[1] = digitalRead(MAIN_POSITIVE_CONTACTOR_RELAY_PIN);
+    contactorsStatusFrame.data[2] = digitalRead(PRECHARGE_CONTACTOR_RELAY_PIN);
+    OVCS_CAN.sendMessage(&contactorsStatusFrame);
   } 
 }
 
 void setup() {
   Serial.begin(9600);
 
-  can_0.reset();
-  can_0.setBitrate(CAN_500KBPS, MCP_8MHZ);
-  can_0.setNormalMode();
+  OVCS_CAN.reset();
+  OVCS_CAN.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  OVCS_CAN.setFilterMask(MCP2515::MASK0,0,0x7FF);
+  OVCS_CAN.setFilter(MCP2515::RXF0,0,CONTACTORS_STATUS_REQUEST_FRAME_ID);
+  OVCS_CAN.setFilterMask(MCP2515::MASK1,0,0x7FF);
+  OVCS_CAN.setNormalMode();
 
-  pinMode(main_negative_contactor_relay, OUTPUT);
-  pinMode(main_positive_contactor_relay, OUTPUT);
-  pinMode(precharge_contactor_relay, OUTPUT);
+  pinMode(MAIN_NEGATIVE_CONTACTOR_RELAY_PIN, OUTPUT);
+  pinMode(MAIN_POSITIVE_CONTACTOR_RELAY_PIN, OUTPUT);
+  pinMode(PRECHARGE_CONTACTOR_RELAY_PIN, OUTPUT);
 
-  contactorStateRequestFrame.can_id = 0x100;
-  contactorStateRequestFrame.can_dlc = 3;
+  contactorsStatusRequestFrame.can_id = CONTACTORS_STATUS_REQUEST_FRAME_ID;
+  contactorsStatusRequestFrame.can_dlc = CONTACTORS_STATUS_REQUEST_FRAME_LENGTH;
 
-  contactorStatusFrame.can_id = 0x101;
-  contactorStatusFrame.can_dlc = 3;
+  contactorsStatusFrame.can_id = CONTACTORS_STATUS_FRAME_ID;
+  contactorsStatusFrame.can_dlc = CONTACTORS_STATUS_FRAME_LENGTH;
+
+  actuate_relays();
 }
 
 void loop() {
