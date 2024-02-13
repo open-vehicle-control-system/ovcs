54901d691b4ca92cbccf9f01357fd98be22f3c1d
Marc Lainez <marc.lainez@gmail.com>
2024-02-13 13:24:11 +0100

    Cast all strings to char* to avoid warnings

--- ovcs_controllers/ovcs_car_controls_controller/test/test_persistance_helpers.h
+++ ovcs_controllers/ovcs_car_controls_controller/test/test_persistance_helpers.h
@@ -5,31 +5,31 @@
 namespace PersistanceHelpersTest{
     void test_update_throttle_high_to_right_address(){
         When(OverloadedMethod(ArduinoFake(EEPROM), update, void(int, uint8_t))).AlwaysReturn();
-        save_calibration_data("throttle_high", 255);
+        save_calibration_data((char*)"throttle_high", 255);
         Verify(Method(ArduinoFake(EEPROM), update).Using(1, 255)).Once();
     }
 
     void test_update_throttle_low_to_right_address(){
         When(OverloadedMethod(ArduinoFake(EEPROM), update, void(int, uint8_t))).AlwaysReturn();
-        save_calibration_data("throttle_low", 0);
+        save_calibration_data((char*)"throttle_low", 0);
         Verify(Method(ArduinoFake(EEPROM), update).Using(0, 0)).Once();
     }
 
     void test_read_throttle_high_reads_to_proper_address(){
         When(OverloadedMethod(ArduinoFake(EEPROM), read, uint8_t(int))).AlwaysReturn();
-        read_calibration_data("throttle_high");
+        read_calibration_data((char*)"throttle_high");
         Verify(Method(ArduinoFake(EEPROM), read).Using(1)).Once();
     }
 
     void test_read_throttle_low_reads_to_proper_address(){
         When(OverloadedMethod(ArduinoFake(EEPROM), read, uint8_t(int))).AlwaysReturn();
-        read_calibration_data("throttle_low");
+        read_calibration_data((char*)"throttle_low");
         Verify(Method(ArduinoFake(EEPROM), read).Using(0)).Once();
     }
 
     void test_read_calibration_data_doesnt_alter_stored_data(){
         When(OverloadedMethod(ArduinoFake(EEPROM), read, uint8_t(int))).Return(255);
-        TEST_ASSERT_EQUAL_INT(255, read_calibration_data("throttle_high"));
+        TEST_ASSERT_EQUAL_INT(255, read_calibration_data((char*)"throttle_high"));
     }
 
     void run_tests(void){
