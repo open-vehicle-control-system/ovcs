0b872da7d4fda1bfbb3239f77d1d8d7c69d48d5a
Marc Lainez <marc.lainez@gmail.com>
2024-02-16 14:45:01 +0100

    Add gear selector tests

--- ovcs_controllers/ovcs_car_controls_controller/test/test_main.cpp
+++ ovcs_controllers/ovcs_car_controls_controller/test/test_main.cpp
@@ -3,8 +3,7 @@
 
 using namespace fakeit;
 
-#include "test_conversion_utils.h"
-#include "test_persistance_helpers.h"
+#include <gear_selector_tests.h>
 
 #define RUN_TEST_GROUP(TEST) \
     if (!std::getenv("TEST_GROUP") || (strcmp(#TEST, std::getenv("TEST_GROUP")) == 0)) { \
@@ -24,6 +23,6 @@ void tearDown(void)
 int main()
 {
   UNITY_BEGIN(); // IMPORTANT LINE!
-  //RUN_TEST_GROUP(CanUtilsTests);
+  RUN_TEST_GROUP(GearSelectorTests);
   return UNITY_END();
 }
