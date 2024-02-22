f1a47d15c42cc44ff1477f2c8b830c859dc5ff28
Marc Lainez <marc.lainez@gmail.com>
2024-02-22 11:14:19 +0100

    Send keep alive message and only trigger gear selection when validated by vms

--- controllers/controls/src/main.cpp
+++ controllers/controls/src/main.cpp
@@ -6,8 +6,12 @@
 #include <ThrottlePedal.h>
 
 #define DEBUGLOG_DEFAULT_LOG_LEVEL_TRACE
+#define STATUS_NOT_INITIALIZED 0
+#define STATUS_INITIALIZED 1
+#define STATUS_ERROR 255
 
 boolean initialized = false;
+int status = STATUS_NOT_INITIALIZED;
 
 Transport transport         = Transport();
 GearSelector gearSelector   = GearSelector();
@@ -26,16 +30,22 @@ void setup() {
     Serial.begin(9600);
     boolean initialized = initializeAllComponents();
     if(!initialized){
-      LOG_ERROR("Controller cannot be initialized"); // TODO : Give more information
+        status = STATUS_ERROR;
+    } else {
+        status = STATUS_INITIALIZED;
     }
 }
 
 void loop() {
+    transport.sendKeepAlive(status);
     int selectedGear = gearSelector.getGearPosition(); // Get which position the gear selector is
     AnalogValues throttlePedalReadings = throttlePedal.readValues(); // Get readings from throttle pedal
-
     // Send frame through transport with aggregated data
     transport.sendFrame(MAX_ANALOG_READ_VALUE, throttlePedalReadings.pin_1, throttlePedalReadings.pin_2, selectedGear);
+    
     int validatedGear = gearIndicator.getValidatedGearPosition(); // Get ECU validated gear
+    if(validatedGear != selectedGear){
+        gearSelector.setGearPosition(validatedGear);
+    }
     gearIndicator.indicateGearPosition(validatedGear); // Light validated gear indicator
 }
