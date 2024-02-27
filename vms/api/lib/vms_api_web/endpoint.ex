4f52acfbc7a9b135978d89da86411524105b0dfc
Marc Lainez <marc.lainez@gmail.com>
2024-02-27 07:32:37 +0100

    Remove uneeded endpoint

--- vms/api/lib/vms_api_web/endpoint.ex
+++ vms/api/lib/vms_api_web/endpoint.ex
@@ -11,10 +11,6 @@ defmodule VmsApiWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
-    longpoll: [connect_info: [session: @session_options]]
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
