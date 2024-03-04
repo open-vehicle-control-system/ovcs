42a135e6bfa79e647e67b2870ba97b37285024af
Marc Lainez <marc.lainez@gmail.com>
2024-03-04 20:06:01 +0100

    Remove apexcharts

--- vms/dashboard/src/main.js
+++ vms/dashboard/src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueApexCharts from "vue3-apexcharts";
 
 import App from './App.vue'
 import router from './router'
@@ -11,7 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VueApexCharts);
 
 
 app.mount('#app')
