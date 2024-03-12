7308da6ddf393ef4f465c7eadb380e075c3c7d48
Marc Lainez <marc.lainez@gmail.com>
2024-03-12 15:11:24 +0100

    Extract network interfaces list in component

---
+++ vms/dashboard/src/components/network_interfaces/List.vue
@@ -0,0 +1,63 @@
+<template>
+    <h3 class="text-base mt-10">Interfaces list</h3>
+    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mt-10">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-300">
+            <thead class="bg-gray-50">
+                <tr>
+                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Link Type</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">State</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Tx queue length</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Load</th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+                <tr v-for="networkInterface in networkInterfaces.interfaces" :key="networkInterface.ifname">
+                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ networkInterface.ifname }}</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ networkInterface.link_type }}</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    <div class="flex items-center gap-x-3">
+                    <div :class="[statuses[networkInterface.operstate], 'flex-none rounded-full']">
+                    <div class="h-2 w-2 rounded-full bg-current"></div>
+                    </div>
+                    <span>{{ networkInterface.operstate }}</span>
+                    </div>
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ networkInterface.txqlen }}</td>
+                <td v-if="networkInterfaces.statistics[networkInterface.ifindex]" class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    <div class="inline-block bg-gray-200 rounded-full h-2.5 dark:bg-gray-100 w-20">
+                    <div class="bg-blue-600 h-2.5 rounded-full" :style="{'width': networkInterfaces.interfaceLoad(networkInterface.ifindex) + '%'}"></div>
+                    </div>
+                    <div class="inline-block px-3">{{ networkInterfaces.interfaceLoad(networkInterface.ifindex) }}%</div>
+                </td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "NetworkInterfaceList",
+    props: ["networkInterfaces"],
+    setup(props) {
+        let networkInterfaces = props.networkInterfaces;
+
+        const statuses = {
+            UNKNOWN: 'text-gray-500 bg-gray-100/10',
+            UP: 'text-green-400 bg-green-400/10',
+            DOWN: 'text-rose-400 bg-rose-400/10',
+        }
+
+        return {
+            networkInterfaces,
+            statuses
+        }
+    }
+}
+
+</script>
--- vms/dashboard/src/views/NetworkView.vue
+++ vms/dashboard/src/views/NetworkView.vue
@@ -6,45 +6,7 @@
         <div class="border-b border-gray-900/10 pb-12">
           <h2 class="text-base font-semibold leading-7 text-gray-900">Network status</h2>
           <div class="mt-8 flow-root">
-            <h3 class="text-base mt-10">Interfaces list</h3>
-            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mt-10">
-              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-                  <table class="min-w-full divide-y divide-gray-300">
-                    <thead class="bg-gray-50">
-                      <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Link Type</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">State</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Tx queue length</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Load</th>
-                      </tr>
-                    </thead>
-                    <tbody class="divide-y divide-gray-200 bg-white">
-                      <tr v-for="networkInterface in networkInterfaces.interfaces" :key="networkInterface.ifname">
-                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ networkInterface.ifname }}</td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ networkInterface.link_type }}</td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <div class="flex items-center gap-x-3">
-                          <div :class="[statuses[networkInterface.operstate], 'flex-none rounded-full']">
-                            <div class="h-2 w-2 rounded-full bg-current"></div>
-                          </div>
-                          <span>{{ networkInterface.operstate }}</span>
-                          </div>
-                        </td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ networkInterface.txqlen }}</td>
-                        <td v-if="networkInterfaces.statistics[networkInterface.ifindex]" class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <div class="inline-block bg-gray-200 rounded-full h-2.5 dark:bg-gray-100 w-20">
-                            <div class="bg-blue-600 h-2.5 rounded-full" :style="{'width': networkInterfaces.interfaceLoad(networkInterface.ifindex) + '%'}"></div>
-                          </div>
-                          <div class="inline-block px-3">{{ networkInterfaces.interfaceLoad(networkInterface.ifindex) }}%</div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
+            <NetworkInterfacesList :networkInterfaces="networkInterfaces"></NetworkInterfacesList>
 
             <h3 class="text-base mt-10">Interfaces traffic</h3>
             <div class="space-y-10 mt-10">
@@ -62,22 +24,19 @@ import { onMounted } from 'vue'
 import { Socket } from 'phoenix'
 
 import NetworkInterfaceStatistics from "../components/network_interfaces/Statistics.vue"
+import NetworkInterfacesList from "../components/network_interfaces/List.vue"
 
 import { useNetworkInterfaces } from "../stores/network_interfaces.js"
 
 export default {
   name: "Network",
   components: {
+    NetworkInterfacesList,
     NetworkInterfaceStatistics
   },
   setup(){
     const networkInterfaces = useNetworkInterfaces()
 
-    const statuses = {
-      UNKNOWN: 'text-gray-500 bg-gray-100/10',
-      UP: 'text-green-400 bg-green-400/10',
-      DOWN: 'text-rose-400 bg-rose-400/10',
-    }
     onMounted(() => {
       let networkInterfacesStore = useNetworkInterfaces()
       let vmsDashboardSocket = new Socket(import.meta.env.VITE_BASE_WS + "/sockets/dashboard", {})
@@ -94,7 +53,6 @@ export default {
 
     return {
       networkInterfaces,
-      statuses
     }
   }
 };
