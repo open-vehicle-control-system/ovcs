901431dba5c1150558d6ad2a764f1ce59e80e8ec
Marc Lainez <marc.lainez@gmail.com>
2024-03-18 14:14:08 +0100

    Remove sticky line on top

--- vms/dashboard/src/App.vue
+++ vms/dashboard/src/App.vue
@@ -1,38 +1,34 @@
 <template>
-    <div class="lg:fixed lg:inset-y-0 lg:flex lg:w-60">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
-        <div class="flex h-16 shrink-0 items-center">
-          <p class="text-xl text-white">OVCS VMS</p>
-        </div>
-        <nav class="flex flex-1 flex-col">
-          <ul role="list" class="flex flex-1 flex-col gap-y-7">
-            <li>
-              <ul role="list" class="-mx-2 space-y-1">
-                <li v-for="item in navigation" :key="item.name">
-                  <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                    <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
-                    {{ item.name }}
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
+  <div class="lg:fixed lg:inset-y-0 lg:flex lg:w-60">
+    <!-- Sidebar component, swap this element with another sidebar if you like -->
+    <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+      <div class="flex h-16 shrink-0 items-center">
+        <p class="text-xl text-white">OVCS VMS</p>
       </div>
+      <nav class="flex flex-1 flex-col">
+        <ul role="list" class="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" class="-mx-2 space-y-1">
+              <li v-for="item in navigation" :key="item.name">
+                <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                  <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+                  {{ item.name }}
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </div>
+  </div>
 
-    <div class="lg:pl-60">
-      <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+  <div class="lg:pl-60">
+    <main class="py-10">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <RouterView />
       </div>
-      <div>
-        <main class="py-10">
-          <div class="px-4 sm:px-6 lg:px-8">
-            <RouterView />
-          </div>
-        </main>
-      </div>
-    </div>
+    </main>
+  </div>
 </template>
 
 <script setup>
