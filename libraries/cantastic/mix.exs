98494b58db0922a1ddc081f695021d1e8bc848ab
Thibault Poncelet <thibault@spin42.com>
2024-03-22 16:10:02 +0100

    Start cantastic repo extract

--- libraries/cantastic/mix.exs
+++ libraries/cantastic/mix.exs
@@ -7,7 +7,8 @@ defmodule Cantastic.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -27,6 +28,13 @@ defmodule Cantastic.MixProject do
       {:decimal, "~> 2.1.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
   end
 end
