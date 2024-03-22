eea6e93736aa035fbaf7d160ec492be8aee91fc3
Marc Lainez <marc.lainez@gmail.com>
2024-03-22 19:21:01 +0100

    Add gear selector component

---
+++ infotainment/dashboard/src/components/controls/GearSelector.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="table text-3xl text-white p-10">
+        <div :class="[store.selectedGear == 'parking' ? 'bg-gray-700 opacity-99' : '', 'table-row']">
+            <span class="h-20 w-20 leading-5 align-middle table-cell text-center">P</span>
+        </div>
+        <div :class="[store.selectedGear == 'reverse' ? 'bg-gray-700 opacity-99 rounded-3xl' : '', 'table-row']">
+            <span class="h-20 w-20 leading-5 align-middle table-cell text-center">R</span>
+        </div>
+        <div :class="[store.selectedGear == 'neutral' ? 'bg-gray-700 opacity-99 rounded-3xl' : '', 'table-row']">
+            <span class="h-20 w-20 leading-5 align-middle table-cell text-center">N</span>
+        </div>
+        <div :class="[store.selectedGear == 'drive' ? 'bg-gray-700 opacity-99 rounded-3xl' : '', 'table-row']">
+            <span class="h-20 w-20 leading-5 align-middle table-cell text-center">D</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["store"])
+const store = props.store
+</script>
