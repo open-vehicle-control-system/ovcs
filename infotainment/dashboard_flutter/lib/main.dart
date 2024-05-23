118982eec3023dc83deb511f53dfdc9520b9c99f
Marc Lainez <marc.lainez@gmail.com>
2024-05-23 12:09:25 +0200

    Make components use google fonts

--- infotainment/dashboard_flutter/lib/main.dart
+++ infotainment/dashboard_flutter/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:dashboard_flutter/components/dashboard_content.dart';
 import 'package:dashboard_flutter/components/side_bar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const MyApp());
 }
 
@@ -14,10 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: _buildTheme(Brightness.dark),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -42,4 +46,15 @@ class _MyHomePageState extends State<MyHomePage> {
       ]
     );
   }
+}
+
+ThemeData _buildTheme(brightness) {
+  var baseTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  );
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
+  );
 }
