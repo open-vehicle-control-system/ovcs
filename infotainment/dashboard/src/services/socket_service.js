20150548b823e1561aeecfeecf6a4e35821c4823
Marc Lainez <marc.lainez@gmail.com>
2024-05-28 11:00:11 +0200

    Remove vue.js infotainment dashboard and related firmware

--- infotainment/dashboard/src/services/socket_service.js
+++
@@ -1,4 +0,0 @@
-import { Socket } from 'phoenix'
-
-export const infotainmentSocket = new Socket(import.meta.env.VITE_BASE_WS + "/sockets/dashboard", {})
-infotainmentSocket.connect();
--- infotainment/firmware/test/nerves_ovcs_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OvcsInfotainmentFirmwareTest do
-  use ExUnit.Case
-  doctest OvcsInfotainmentFirmware
-
-  test "greets the world" do
-    assert OvcsInfotainmentFirmwareTest.hello() == :world
-  end
-end
