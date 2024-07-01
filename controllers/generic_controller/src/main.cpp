60876f0a32310d87a2f4cbc66f29546c34327eeb
Thibault Poncelet <thibault@spin42.com>
2024-07-01 16:26:09 +0200

    Store and load configuration on EEPROM together with CRC

--- controllers/generic_controller/src/main.cpp
+++ controllers/generic_controller/src/main.cpp
@@ -12,16 +12,13 @@
 #define PIN_STATUS_FRAME_FREQUENCY_MS 10
 #define ADOPTION_BUTTON_PIN 2
 #define ADOPTION_FRAME_ID 0x700
-#define CONTROLLER_ID_EEPROM_ADDRESS 0
-#define CONTROLLER_ID_CRC_EEPROM_ADDRESS 16
-#define DIGITAL_PIN_CONFIGURATION_EEPROM_ADDRESS 48
-#define DIGITAL_PIN_CONFIGURATION_CRC_EEPROM_ADDRESS 112
-#define OTHER_PIN_CONFIGURATION_EEPROM_ADDRESS 144
-#define OTHER_PIN_CONFIGURATION_CRC_EEPROM_ADDRESS 152
+#define CONFIGURATION_EEPROM_ADDRESS 0
+#define CONFIGURATION_CRC_EEPROM_ADDRESS 64
 #define ALIVE_FRAME_ID_MASK 0x700
 #define DIGITAL_PIN_REQUEST_FRAME_ID_MASK 0x701
 #define OTHER_PIN_REQUEST_FRAME_ID_MASK 0x702
 #define DIGITAL_AND_ANALOG_PIN_STATUS_FRAME_ID_MASK 0x703
+#define CONFIGURATION_BYTE_SIZE 8
 
 MCP23008 I2C_MOSFET_1(0x27);
 MCP23008 I2C_MOSFET_2(0x26);
@@ -45,6 +42,7 @@ uint16_t digitalAndAnalogPinStatusFrameId;
 
 bool waitingAdoption = false;
 bool ready = false;
+bool adoptedSinceLastBoot = false;
 
 CANMessage receivedFrame;
 
@@ -97,7 +95,9 @@ void receiveFrame() {
 }
 
 bool isWaitingAdoption() {
-  if (waitingAdoption) {
+  if (adoptedSinceLastBoot) {
+    return false;
+  } else if (waitingAdoption) {
     return true;
   } else {
     waitingAdoption = digitalRead(ADOPTION_BUTTON_PIN) == HIGH;
@@ -109,14 +109,13 @@ void initializeAdoptionButton() {
   pinMode(ADOPTION_BUTTON_PIN, INPUT);
 }
 
-void storeId(CANMessage frame) {
-  // Store Id + CRC on EEPROM
-}
-void storeDigitalPinConfiguration(CANMessage frame) {
-  // Store digital Pin config + CRC on EEPROM
-}
-void storeOtherPinConfiguration(CANMessage frame) {
-  // Store other PIN + CRC on EEPROM
+void storeConfiguration(CANMessage frame) {
+  uint32_t crc = CRC32::calculate(frame.data, CONFIGURATION_BYTE_SIZE);
+  Serial.println("Saving");
+  Serial.println(crc);
+  EEPROM.put(CONFIGURATION_EEPROM_ADDRESS, frame.data64);
+  EEPROM.put(CONFIGURATION_CRC_EEPROM_ADDRESS, crc);
+  adoptedSinceLastBoot = true;
 }
 
 void setDigitalPins(CANMessage receivedFrame) {
@@ -153,39 +152,13 @@ uint16_t computeFrameId(uint8_t controllerId, uint16_t mask) {
 }
 void loadConfiguration() {
   uint32_t crc;
-  // Controller ID
-  bool controllerIdValid = false;
-  uint32_t controllerIdCrc;
-  size_t controllerIdByteSize = 2;
-  EEPROM.get(CONTROLLER_ID_EEPROM_ADDRESS, controllerId);
-  EEPROM.get(CONTROLLER_ID_CRC_EEPROM_ADDRESS, controllerIdCrc);
-  crc = CRC32::calculate(&controllerId, controllerIdByteSize);
-  if (crc == controllerIdCrc) {
-    controllerIdValid = true;
-  }
-  // Digital Pin Configuration
-  bool digitalPinConfigurationValid = false;
-  uint32_t digitalPinConfigurationCrc;
-  size_t digitalPinConfigurationSize = 8;
-  EEPROM.get(DIGITAL_PIN_CONFIGURATION_EEPROM_ADDRESS, digitalPinConfiguration);
-  EEPROM.get(DIGITAL_PIN_CONFIGURATION_CRC_EEPROM_ADDRESS, digitalPinConfigurationCrc);
-  crc = CRC32::calculate(&controllerId, digitalPinConfigurationSize);
-  if (crc == digitalPinConfigurationCrc) {
-    digitalPinConfigurationValid = true;
-  }
-
-  // Other Pin Configuration
-  bool otherPinConfigurationValid = false;
-  uint32_t otherPinConfigurationCrc;
-  size_t otherPinConfigurationByteSize = 8;
-  EEPROM.get(DIGITAL_PIN_CONFIGURATION_EEPROM_ADDRESS, otherPinConfiguration);
-  EEPROM.get(DIGITAL_PIN_CONFIGURATION_CRC_EEPROM_ADDRESS, otherPinConfigurationCrc);
-  crc = CRC32::calculate(&controllerId, otherPinConfigurationByteSize);
-  if (crc == otherPinConfigurationCrc) {
-    otherPinConfigurationValid = true;
-  }
-
-  if (controllerIdValid && digitalPinConfigurationValid && otherPinConfigurationValid) {
+  uint32_t configurationCrc;
+  size_t configurationByteSize = 8;
+  uint8_t configuration [8];
+  EEPROM.get(CONFIGURATION_EEPROM_ADDRESS, configuration);
+  EEPROM.get(CONFIGURATION_CRC_EEPROM_ADDRESS, configurationCrc);
+  crc = CRC32::calculate(configuration, CONFIGURATION_BYTE_SIZE);
+  if (crc == configurationCrc) {
     Serial.println("Saved configuration loaded, ready!");
     aliveFrameId                     = computeFrameId(controllerId, ALIVE_FRAME_ID_MASK);
     digitalPinRequestFrameId         = computeFrameId(controllerId, DIGITAL_PIN_REQUEST_FRAME_ID_MASK);
@@ -193,14 +166,13 @@ void loadConfiguration() {
     digitalAndAnalogPinStatusFrameId = computeFrameId(controllerId, DIGITAL_AND_ANALOG_PIN_STATUS_FRAME_ID_MASK);
     ready                            = true;
   } else {
+    ready = false;
     Serial.println("Saved configuration invalid, adoption required to continue.");
   }
 }
 
 void adoptConfiguration(CANMessage frame) {
-  storeId(frame);
-  storeDigitalPinConfiguration(frame);
-  storeOtherPinConfiguration(frame);
+  storeConfiguration(frame);
   loadConfiguration();
   waitingAdoption = false;
 };
@@ -217,7 +189,7 @@ void setup()
 void loop () {
   receiveFrame();
   if (isWaitingAdoption() && receivedFrame.id == ADOPTION_FRAME_ID) {
-    Serial.println("Adoption started");
+    Serial.println("--> Adoption started <--");
     adoptConfiguration(receivedFrame);
   } else if (ready) {
     if (receivedFrame.id == digitalPinRequestFrameId) {
