3e7f92656a6e40bc4362e33c0497188acae567b7
Thibault Poncelet <thibault@spin42.com>
2024-07-12 12:28:19 +0200

    Test digital pin init

--- controllers/generic_controller/test/DigitalPinTest.h
+++ controllers/generic_controller/test/DigitalPinTest.h
@@ -3,16 +3,61 @@
 #include <MockBoard.h>
 
 namespace DigitalPinTests{
-    void tesDigitalWriteIfAllowedWhenDisabled(){
-         MockBoard mockBoard = MockBoard();
-         DigitalPin digitalPin = DigitalPin(0, &mockBoard, 0);
+    void tesDigitalInitializationWhenDisabled() {
+        MockBoard mockBoard = MockBoard();
+        Mock<MockBoard> spy(mockBoard);
+        When(Method(spy, pinMode)).Return();
+        DigitalPin digitalPin = DigitalPin(0, &mockBoard, 0);
+        Verify(Method(spy, pinMode).Using(0, OUTPUT)).Exactly(0);
+    }
+
+    void tesDigitalInitializationWhenReadOnly() {
+        MockBoard mockBoard = MockBoard();
+        Mock<MockBoard> spy(mockBoard);
+        When(Method(spy, pinMode)).Return();
+        DigitalPin digitalPin = DigitalPin(1, &mockBoard, 0);
+        Verify(Method(spy, pinMode).Using(0, INPUT)).Exactly(1);
+    }
+
+    void tesDigitalInitializationWhenWriteOnly() {
+        MockBoard mockBoard = MockBoard();
+        Mock<MockBoard> spy(mockBoard);
+        When(Method(spy, pinMode)).Return();
+        When(Method(spy, digitalWrite)).Return();
+        DigitalPin digitalPin = DigitalPin(2, &mockBoard, 0);
+        Verify(Method(spy, pinMode).Using(0, OUTPUT)).Exactly(1);
+        Verify(Method(spy, digitalWrite).Using(0, 0)).Exactly(1);
+    }
+
+    void tesDigitalInitializationWhenReadWrite() {
+        MockBoard mockBoard = MockBoard();
+        Mock<MockBoard> spy(mockBoard);
+        When(Method(spy, pinMode)).Return();
+        When(Method(spy, digitalWrite)).Return();
+        DigitalPin digitalPin = DigitalPin(3, &mockBoard, 0);
+        Verify(Method(spy, pinMode).Using(0, OUTPUT)).Exactly(1);
+        Verify(Method(spy, digitalWrite).Using(0, 0)).Exactly(1);
+    }
+
+    void tesDigitalWriteIfAllowedWhenDisabled() {
+        MockBoard mockBoard = MockBoard();
+        DigitalPin digitalPin = DigitalPin(0, &mockBoard, 0);
         Mock<MockBoard> spy(mockBoard);
         When(Method(spy, digitalWrite)).Return();
         digitalPin.writeIfAllowed(1);
         Verify(Method(spy, digitalWrite).Using(0, 1)).Exactly(0);
     }
 
-    void testDigitalWriteIfAllowedWhenWriteOnly(){
+    void testDigitalWriteIfAllowedWhenReadOnly() {;
+        MockBoard mockBoard = MockBoard();
+        DigitalPin digitalPin = DigitalPin(1, &mockBoard, 4);
+        Mock<MockBoard> spy(mockBoard);
+        When(Method(spy, digitalWrite)).Return();
+        digitalPin.writeIfAllowed(1);
+        Verify(Method(spy, digitalWrite).Using(4, 1)).Exactly(0);
+    }
+
+    void testDigitalWriteIfAllowedWhenWriteOnly() {
         MockBoard mockBoard = MockBoard();
         DigitalPin digitalPin = DigitalPin(2, &mockBoard, 2);
         Mock<MockBoard> spy(mockBoard);
@@ -21,7 +66,7 @@ namespace DigitalPinTests{
         Verify(Method(spy, digitalWrite).Using(2, 1)).Exactly(1);
     }
 
-    void testDigitalWriteIfAllowedWhenReadWrite(){
+    void testDigitalWriteIfAllowedWhenReadWrite() {
         MockBoard mockBoard = MockBoard();
         DigitalPin digitalPin = DigitalPin(3, &mockBoard, 3);
         Mock<MockBoard> spy(mockBoard);
@@ -30,20 +75,14 @@ namespace DigitalPinTests{
         Verify(Method(spy, digitalWrite).Using(3, 0)).Exactly(1);
     }
 
-    void testDigitalWriteIfAllowedWhenReadOnly(){;
-        MockBoard mockBoard = MockBoard();
-        DigitalPin digitalPin = DigitalPin(3, &mockBoard, 4);
-        Mock<MockBoard> spy(mockBoard);
-        When(Method(spy, digitalWrite)).Return();
-        digitalPin.writeIfAllowed(1);
-        Verify(Method(spy, digitalWrite).Using(4, 0)).Exactly(0);
-    }
-
-
     void run_tests(void){
+        RUN_TEST(tesDigitalInitializationWhenDisabled);
+        RUN_TEST(tesDigitalInitializationWhenWriteOnly);
+        RUN_TEST(tesDigitalInitializationWhenReadOnly);
+        RUN_TEST(tesDigitalInitializationWhenReadWrite);
         RUN_TEST(tesDigitalWriteIfAllowedWhenDisabled);
+        RUN_TEST(testDigitalWriteIfAllowedWhenReadOnly);
         RUN_TEST(testDigitalWriteIfAllowedWhenWriteOnly);
         RUN_TEST(testDigitalWriteIfAllowedWhenReadWrite);
-        RUN_TEST(testDigitalWriteIfAllowedWhenReadOnly);
     }
 }
