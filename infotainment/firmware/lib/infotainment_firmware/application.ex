f297d7c56ee133d6421307d767ad2cf4256bc729
Marc Lainez <marc.lainez@gmail.com>
2024-08-02 16:19:00 +0200

    Use uevents to wait for drm to be initialized instead of a single long sleep

--- infotainment/firmware/lib/infotainment_firmware/application.ex
+++ infotainment/firmware/lib/infotainment_firmware/application.ex
@@ -4,13 +4,19 @@ defmodule InfotainmentFirmware.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
+    wait_for_drm()
+  end
+
+  def target() do
+    Application.get_env(:infotainment_firmware, :target)
+  end
+
+  def start_infotainment() do
     opts = [strategy: :rest_for_one, name: Example.Supervisor]
-
-    Process.sleep(10_000)
-
     children = [
       {NervesFlutterpi,
         flutter_app_dir: "/var/flutter_assets",
@@ -19,7 +25,13 @@ defmodule InfotainmentFirmware.Application do
     Supervisor.start_link(children, opts)
   end
 
-  def target() do
-    Application.get_env(:infotainment_firmware, :target)
+  def wait_for_drm() do
+    if NervesUEvent.get(["devices", "platform", "gpu", "drm", "card1"]) == nil do
+      Logger.info("Waiting for DRM device to be ready")
+      Process.sleep(500)
+      wait_for_drm()
+    else
+      start_infotainment()
+    end
   end
 end
