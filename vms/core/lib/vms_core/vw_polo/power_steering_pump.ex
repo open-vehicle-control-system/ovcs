9f1c37e1f32dde54323a4be37916ab1af34d1aed
Thibault Poncelet <thibault@spin42.com>
2024-08-05 11:07:25 +0200

    Add abs status frame to misc can network for power steering pump

--- vms/core/lib/vms_core/vw_polo/power_steering_pump.ex
+++ vms/core/lib/vms_core/vw_polo/power_steering_pump.ex
@@ -1,10 +1,18 @@
 defmodule VmsCore.VwPolo.PowerSteeringPump do
   use GenServer
-  alias Cantastic.{Emitter}
+  alias Cantastic.{Emitter, Frame, Receiver}
+
+  @polo_network_name :polo_drive
+  @abs_status_frame_name "abs_status"
 
   @network_name :misc
-
   @engine_status_frame_name "engine_status"
+
+  @impl true
+  def handle_info({:handle_frame,  %Frame{name: @abs_status_frame_name} = frame}, state) do
+    :ok = Emitter.forward(@network_name, frame)
+    {:noreply, state}
+  end
 
   @impl true
   def init(_) do
@@ -14,6 +22,7 @@ defmodule VmsCore.VwPolo.PowerSteeringPump do
         "engine_rotations_per_minute" => 0
       }
     })
+    :ok = Receiver.subscribe(self(), @polo_network_name, @abs_status_frame_name)
     {:ok, %{}}
   end
 
