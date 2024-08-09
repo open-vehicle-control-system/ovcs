bef639e1061784dedd98ac8aaf7fcff8e3473d06
Thibault Poncelet <thibault@spin42.com>
2024-08-09 16:09:48 +0200

    fallback on device symlink when nerves uevent is not returning the expected events for can interfaces

--- vms/firmware/lib/vms_firmware/util/network_mapper.ex
+++ vms/firmware/lib/vms_firmware/util/network_mapper.ex
@@ -5,22 +5,38 @@ defmodule VmsFirmware.Util.NetworkMapper do
     spi_mappings
     |> String.split(",", trim: true)
     |> Enum.map(fn(i) -> i |> String.split(":", trim: true) end)
-    |> Enum.map(fn([network_name, spi_interface]) ->
-        Task.async(fn() -> wait_for_spi(network_name, spi_interface) end)
+    |> Enum.map(fn([network_name, interface]) ->
+        Task.async(fn() -> wait_for_interface(network_name, interface) end)
       end)
-    |> Task.await_many(12000)
+    |> Task.await_many(20000)
   end
 
-  defp wait_for_spi(network_name, spi_interface) do
+  defp wait_for_interface(network_name, spi_interface) when binary_part(spi_interface, 0, 3) == "spi" do
     NervesUEvent.subscribe(["devices", "platform", "soc", :_, "spi_master", :_, spi_interface])
-    Logger.info("Waiting for SPI #{spi_interface} device to be ready...")
-    receive do
-      %PropertyTable.Event{table: NervesUEvent, property: _, value: %{"ifindex" => _, "interface" => can_interface, "subsystem" => "net"}} = value ->
-        Logger.info("SPI interface #{spi_interface} is ready and assigned to: #{network_name} => #{can_interface}")
-        {network_name, can_interface}
-    after
-      10000 ->
-        throw "SPI interface  '#{spi_interface}' to be used for '#{network_name}' not ready within 5 seconds, aborting!"
+    match = NervesUEvent.match(["devices", "platform", "soc", :_, "spi_master", :_, spi_interface, "net", :_, :"$"])
+    case match do
+      [] ->
+        Logger.info("Waiting for SPI #{spi_interface} device to be ready...")
+        receive do
+          %PropertyTable.Event{table: NervesUEvent, property: _, value: %{"ifindex" => _, "interface" => can_interface, "subsystem" => "net"}} = value ->
+            Logger.info("SPI interface #{spi_interface} is ready and assigned to: #{network_name} => #{can_interface}")
+            {network_name, can_interface}
+        after
+          5000 ->
+            case File.ls("/sys/bus/spi/devices/#{spi_interface}/net") do
+              {:ok, [can_interface]} ->
+                Logger.info("SPI interface #{spi_interface} is ready according to /sys/bus/spi/devices/ and assigned to: #{network_name} => #{can_interface}")
+                {network_name, can_interface}
+              _ ->
+                throw "SPI interface  '#{spi_interface}' to be used for '#{network_name}' not ready within 5 seconds, aborting!"
+            end
+        end
+      [{_, %{"interface" => existing_can_interface}}] ->
+        Logger.info("SPI interface #{spi_interface} was already and assigned to: #{network_name} => #{existing_can_interface}")
+        {network_name, existing_can_interface}
     end
+  end
+  defp wait_for_interface(network_name, interface) do
+    {network_name, interface}
   end
 end
