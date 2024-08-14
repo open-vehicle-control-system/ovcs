6db797b49aa2c0883fed52675d82238374d174f5
Thibault Poncelet <thibault@spin42.com>
2024-08-14 16:50:01 +0200

    Adapt txqueuelen

--- scripts/setup_arduino_can.sh
+++ scripts/setup_arduino_can.sh
@@ -6,8 +6,11 @@ sudo ip link set down can0
 sudo ip link set down can1
 sudo ip link set down can2
 sudo ip link set can0 type can bitrate 500000
+sudo ip link set can0 txqueuelen 10000
 sudo ip link set can1 type can bitrate 500000
+sudo ip link set can1 txqueuelen 10000
 sudo ip link set can2 type can bitrate 500000
+sudo ip link set can2 txqueuelen 10000
 sudo ip link set up can0
 sudo ip link set up can1
 sudo ip link set up can2
