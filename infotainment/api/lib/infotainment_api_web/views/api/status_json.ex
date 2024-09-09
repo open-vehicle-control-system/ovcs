9ec4e00b2c57e16964afb9c0bde2ebcc3fd94b62
Marc Lainez <marc.lainez@gmail.com>
2024-09-09 13:59:34 +0200

    Create system status json view

---
+++ infotainment/api/lib/infotainment_api_web/views/api/status_json.ex
@@ -0,0 +1,21 @@
+defmodule InfotainmentApiWeb.Api.StatusJSON do
+  use InfotainmentApiWeb, :view
+
+  def render("status.json", status) do
+    %{
+      type: "status",
+      id:    "status",
+      attributes: %{
+        bms_missing: status.bms_missing,
+        vms_missing: status.vms_missing,
+        front_controller_missing: status.front_controller_missing,
+        rear_controller_missing: status.rear_controller_missing,
+        inverter_missing: status.inverter_missing,
+        controls_controller_missing: status.controls_controller_missing,
+        main_negative_off: status.main_negative_off,
+        main_positive_off: status.main_negative_off,
+        precharge_off: status.precharge_off,
+      }
+    }
+  end
+end
