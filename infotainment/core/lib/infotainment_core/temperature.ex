70eb068675713dc22c3adf8baf632753654fae08
Marc Lainez <marc.lainez@gmail.com>
2024-09-10 14:43:14 +0200

    Do nothing when the thermal file does not exist and prevent output in console

--- infotainment/core/lib/infotainment_core/temperature.ex
+++ infotainment/core/lib/infotainment_core/temperature.ex
@@ -15,9 +15,13 @@ defmodule InfotainmentCore.Temperature do
 
   @impl true
   def handle_call(:temperature, _from, state) do
-    {content, 0} = System.cmd("cat", ["/sys/class/thermal/thermal_zone0/temp"])
-    {temperature_1000, _} = Integer.parse(content)
-    state = %{state | temperature: temperature_1000/1000.0}
+    state = case System.cmd("cat", ["/sys/class/thermal/thermal_zone0/temp"], stderr_to_stdout: true) do
+      {content, 0} ->
+        {temperature_1000, _} = Integer.parse(content)
+        %{state | temperature: temperature_1000/1000.0}
+      {_, 1} ->
+        state
+    end
     {:reply, {:ok, state}, state}
   end
 
