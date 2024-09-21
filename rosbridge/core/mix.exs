2f53c588fcedc8d280289752b70841827bfec2f1
Marc Lainez <marc.lainez@gmail.com>
2024-09-21 17:18:27 +0200

    Add main application

--- rosbridge/core/mix.exs
+++ rosbridge/core/mix.exs
@@ -14,6 +14,7 @@ defmodule OvcsRosBridgeCore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {OvcsRosBridgeCore.Application, []},
       extra_applications: [:logger]
     ]
   end
