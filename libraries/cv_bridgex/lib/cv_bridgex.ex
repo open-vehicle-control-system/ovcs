f4af47ce1246f70799cdc8869254388c81968788
Marc Lainez <marc.lainez@gmail.com>
2024-09-30 11:17:59 +0200

    Add cv_bridgex library boilerplate

---
+++ libraries/cv_bridgex/lib/cv_bridgex.ex
@@ -0,0 +1,18 @@
+defmodule CvBridgex do
+  @moduledoc """
+  Documentation for `CvBridgex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CvBridgex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ libraries/cv_bridgex/test/cv_bridgex_test.exs
@@ -0,0 +1,8 @@
+defmodule CvBridgexTest do
+  use ExUnit.Case
+  doctest CvBridgex
+
+  test "greets the world" do
+    assert CvBridgex.hello() == :world
+  end
+end
