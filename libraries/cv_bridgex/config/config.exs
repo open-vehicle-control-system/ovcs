49bbc63807ee0ddffa8bfd83ea38934eb869e6e2
Marc Lainez <marc.lainez@gmail.com>
2024-10-02 16:51:42 +0200

    Use default config for forks. Don't know how to get rid of srvs and actions for now.

--- libraries/cv_bridgex/config/config.exs
+++ libraries/cv_bridgex/config/config.exs
@@ -1,3 +1,12 @@
 import Config
 
-config :rclex, ros2_message_types: ["sensor_msgs/msg/Image", "std_msgs/msg/Header"]
+config :rclex,
+  ros2_message_types: [
+    "sensor_msgs/msg/Image", "std_msgs/msg/Header"
+  ],
+  ros2_service_types: [
+    "std_srvs/srv/SetBool"
+  ],
+  ros2_action_types: [
+    "turtlesim/action/RotateAbsolute"
+  ]
