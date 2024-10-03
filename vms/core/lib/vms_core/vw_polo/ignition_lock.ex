392918717517ed219fc4c4170194ea57636b6722
Thibault Poncelet <thibault@spin42.com>
2024-10-03 14:59:57 +0200

    Migrate ignition lock to Vms Bus

--- vms/core/lib/vms_core/vw_polo/ignition_lock.ex
+++ vms/core/lib/vms_core/vw_polo/ignition_lock.ex
@@ -2,38 +2,37 @@ defmodule VmsCore.VwPolo.IgnitionLock do
   use GenServer
   alias Cantastic.{Frame, Signal}
 
-  @network_name :polo_drive
-
-  @key_status_frame_name "key_status"
-
-  @impl true
-  def init(_) do
-    Cantastic.Receiver.subscribe(self(), @network_name, @key_status_frame_name)
-    {:ok, %{
-      key_status: "off"
-    }}
-  end
+  @loop_period 10
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  # off
-  # key_engaged
-  # contact_on
-  # start_engine
+  @impl true
+  def init(_) do
+    Cantastic.Receiver.subscribe(self(), :polo_drive, "key_status")
+    {:ok, timer} = :timer.send_interval(@loop_period, :loop)
+    {:ok, %{
+      contact: :off,
+      loop_timer: timer
+    }}
+  end
+
+  @impl true
+  def handle_info(:loop, state) do
+    VmsCore.Bus.broadcast("messages", %VmsCore.Bus.Message{name: :contact, value: state.contact})
+    {:noreply, state}
+  end
+
   @impl true
   def handle_info({:handle_frame, %Frame{signals: signals}}, state) do
     %{"key_state" => %Signal{value: key_status}} = signals
-    {:noreply, %{state | key_status: key_status}}
-  end
-
-  @impl true
-  def handle_call(:key_status, _from, state) do
-    {:reply, {:ok, state.key_status}, state}
-  end
-
-  def key_status() do
-    GenServer.call(__MODULE__, :key_status)
+    contact = case key_status do
+      "contact_off" -> :off
+      "key_engaged" -> :off
+      "contact_on"  -> :on
+      "start_engine" -> :start
+    end
+    {:noreply, %{state | contact: contact}}
   end
 end
