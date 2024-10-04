f8061d87c2a27d3507990ee4bf45c2d3a1539818
Marc Lainez <marc.lainez@gmail.com>
2024-10-04 10:18:54 +0200

    Get config and create one process per camera

--- libraries/cv_bridgex/lib/cv_bridgex/application.ex
+++ libraries/cv_bridgex/lib/cv_bridgex/application.ex
@@ -5,11 +5,17 @@ defmodule CvBridgex.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      {CvBridgex.CvCamera, []},
-      {CvBridgex.RosImageEmitter, []}
-    ]
-
+    cameras = Application.get_env(:cv_bridgex, :cameras)
+    children = Enum.map(cameras, fn camera ->
+      %{
+        id: camera.process_name,
+        start: {
+          CvBridgex.CvCamera,
+          :start_link, [%{process_name: camera.process_name, device: camera.device_id}]
+        }
+      }
+    end)
+    children = [{CvBridgex.RosImageEmitter, cameras} | children]
     opts = [strategy: :one_for_one, name: CvBridgex.Supervisor]
     Supervisor.start_link(children, opts)
   end
