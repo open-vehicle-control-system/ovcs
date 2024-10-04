41d0f026a929db5afe852aca733225be1bfe7883
Marc Lainez <marc.lainez@gmail.com>
2024-10-04 10:17:19 +0200

    Add config to specify multiple cameras

--- libraries/cv_bridgex/config/config.exs
+++ libraries/cv_bridgex/config/config.exs
@@ -4,3 +4,17 @@ config :rclex,
   ros2_message_types: [
     "sensor_msgs/msg/Image", "std_msgs/msg/Header"
   ]
+
+  config :cv_bridgex,
+    cameras: [
+      %{
+        process_name: FrontLeftCamera,
+        device_id: 0,
+        topic: "/front_left_camera"
+      },
+      %{
+        process_name: FrontRightCamera,
+        device_id: 2,
+        topic: "/front_right_camera"
+      }
+    ]
