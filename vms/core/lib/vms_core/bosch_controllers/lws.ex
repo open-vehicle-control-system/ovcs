8d4060b98af38bfb60e2cb6e26ec485e740046b7
Thibault Poncelet <thibault@spin42.com>
2024-10-07 10:31:40 +0200

    Migrate LWS sensor to the bus

--- vms/core/lib/vms_core/bosch_controllers/lws.ex
+++ vms/core/lib/vms_core/bosch_controllers/lws.ex
@@ -2,37 +2,42 @@ defmodule VmsCore.Bosch.Lws do
   use GenServer
   alias Cantastic.{Emitter, Receiver, Frame, Signal}
   alias Decimal, as: D
-
-  @network_name :misc
-
-  @lws_status_frame_name "lws_status"
-  @lws_config_frame_name "lws_config"
+  alias VmsCore.Bus
 
   @zero D.new(0)
-
-  @impl true
-  def init(_) do
-    :ok = Emitter.configure(@network_name, @lws_config_frame_name, %{
-      parameters_builder_function: :default,
-      initial_data: %{
-        "command" => "reset_angle_calibration_status",
-      }
-    })
-    :ok = Receiver.subscribe(self(), @network_name, [@lws_status_frame_name])
-    {:ok, %{
-      angle: @zero,
-      angular_speed: @zero,
-      trimming_valid: false,
-      calibration_valid: false,
-      sensor_ready: false
-    }}
-  end
+  @loop_period 10
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   @impl true
+  def init(_) do
+    :ok = Emitter.configure(:misc, "lws_config", %{
+      parameters_builder_function: :default,
+      initial_data: %{
+        "command" => "reset_angle_calibration_status",
+      }
+    })
+    :ok = Receiver.subscribe(self(), :misc, ["lws_status"])
+    {:ok, timer} = :timer.send_interval(@loop_period, :loop)
+    {:ok, %{
+      angle: @zero,
+      angular_speed: @zero,
+      trimming_valid: false,
+      calibration_valid: false,
+      sensor_ready: false,
+      loop_timer: timer
+    }}
+  end
+
+  @impl true
+  def handle_info(:loop, state) do
+    state = state
+      |> emit_metrics()
+    {:noreply, state}
+  end
+
   def handle_info({:handle_frame, %Frame{signals: signals}}, state) do
     %{
       "steering_wheel_angle" => %Signal{value: angle},
@@ -52,15 +57,14 @@ defmodule VmsCore.Bosch.Lws do
     }
   end
 
-  @impl true
-  def handle_call(:state, _from, state) do
-    {:reply, {:ok, state}, state}
+  defp emit_metrics(state) do
+    Bus.broadcast("messages", %Bus.Message{name: :angle, value: state.angle, source: __MODULE__})
+    Bus.broadcast("messages", %Bus.Message{name: :angular_speed, value: state.angular_speed, source: __MODULE__})
+    Bus.broadcast("messages", %Bus.Message{name: :trimming_valid, value: state.trimming_valid, source: __MODULE__})
+    Bus.broadcast("messages", %Bus.Message{name: :calibration_valid, value: state.calibration_valid, source: __MODULE__})
+    Bus.broadcast("messages", %Bus.Message{name: :sensor_ready, value: state.sensor_ready, source: __MODULE__})
+    state
   end
-
-  def status() do
-    GenServer.call(__MODULE__, :state)
-  end
-
 
   def calibrate_angle_0 do
     :ok = reset_angle_calibration_status()
@@ -69,20 +73,20 @@ defmodule VmsCore.Bosch.Lws do
   end
 
   defp reset_angle_calibration_status do
-    :ok = Emitter.update(@network_name, @lws_config_frame_name, fn (data) ->
+    :ok = Emitter.update(:misc, "lws_config", fn (data) ->
       %{data | "command" => "reset_angle_calibration_status"}
     end)
-    :ok = Emitter.enable(@network_name, @lws_config_frame_name)
+    :ok = Emitter.enable(:misc, "lws_config")
     :timer.sleep(500)
-    :ok = Emitter.disable(@network_name, @lws_config_frame_name)
+    :ok = Emitter.disable(:misc, "lws_config")
   end
 
   defp set_angle_0 do
-    :ok = Emitter.update(@network_name, @lws_config_frame_name, fn (data) ->
+    :ok = Emitter.update(:misc, "lws_config", fn (data) ->
       %{data | "command" => "set_angle_zero"}
     end)
-    :ok = Emitter.enable(@network_name, @lws_config_frame_name)
+    :ok = Emitter.enable(:misc, "lws_config")
     :timer.sleep(500)
-    :ok = Emitter.disable(@network_name, @lws_config_frame_name)
+    :ok = Emitter.disable(:misc, "lws_config")
   end
 end
