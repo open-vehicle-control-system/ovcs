6b4f78051d3ccbaa780d79ce73ef6ca728ab9505
Thibault Poncelet <thibault@spin42.com>
2024-10-07 10:15:20 +0200

    Emit contactors statuses values on the bus

--- vms/core/lib/vms_core/high_voltage_contactors.ex
+++ vms/core/lib/vms_core/high_voltage_contactors.ex
@@ -35,7 +35,10 @@ defmodule VmsCore.HighVoltageContactors do
       inverter_output_voltage_source: inverter_output_voltage_source,
       required_precharge_output_voltage: required_precharge_output_voltage,
       precharge_ending_timestamp: 0,
-      loop_timer: timer
+      loop_timer: timer,
+      main_negative_relay_enabled: false,
+      main_positive_relay_enabled: false,
+      precharge_relay_enabled: false
     }}
   end
 
@@ -43,6 +46,8 @@ defmodule VmsCore.HighVoltageContactors do
   def handle_info(:loop, state) do
     state = state
       |> toggle_contactors()
+      |> fetch_status()
+      |> emit_metrics()
     {:noreply, state}
   end
 
@@ -60,23 +65,58 @@ defmodule VmsCore.HighVoltageContactors do
     now = System.system_time(:millisecond)
     case {state.status, state.contact, state.inverter_output_voltage, state.precharge_ending_timestamp} do
       {:off, :start, _, _} ->
-        :ok = enable_relay(state, :main_negative_relay_pin)
-        :ok = enable_relay(state, :precharge_relay_pin)
-        %{state | status: :starting}
+        start_precharge(state)
       {:starting, contact, inverter_output_voltage, _} when contact in [:on, :start] and inverter_output_voltage >= state.required_precharge_output_voltage ->
-        :ok = enable_relay(state, :main_positive_relay_pin)
-        precharge_ending_timestamp = System.system_time(:millisecond) + @relay_operating_delay
-        %{state | status: :precharge_complete, precharge_ending_timestamp: precharge_ending_timestamp}
+        end_precharge(state)
       {:precharge_complete, contact, _, precharge_ending_timestamp} when contact in [:on, :start] and precharge_ending_timestamp > now ->
-        :ok = disable_relay(state, :precharge_relay_pin)
-        %{state | status: :started}
+        finish_precharge(state)
       {status, :off, _, _} when status != :off ->
-        :ok = disable_relay(state, :precharge_relay_pin)
-        :ok = disable_relay(state, :main_negative_relay_pin)
-        :ok = disable_relay(state, :main_positive_relay_pin)
-        %{state | status: :off}
-      _ -> state
+        switch_off(state)
+      _ ->
+        state
     end
+  end
+
+  defp fetch_status(state) do
+    {:ok, main_negative_relay_enabled} = VmsCore.Controllers.GenericController.get_digital_value(state.controller, state.main_negative_relay_pin)
+    {:ok, main_positive_relay_enabled} = VmsCore.Controllers.GenericController.get_digital_value(state.controller, state.main_positive_relay_pin)
+    {:ok, precharge_relay_enabled}     = VmsCore.Controllers.GenericController.get_digital_value(state.controller, state.precharge_relay_pin)
+    %{state |
+      main_negative_relay_enabled: main_negative_relay_enabled,
+      main_positive_relay_enabled: main_positive_relay_enabled,
+      precharge_relay_enabled: precharge_relay_enabled
+    }
+  end
+
+  defp emit_metrics(state) do
+    Bus.broadcast("messages", %Bus.Message{name: :main_negative_relay_enabled, value: state.main_negative_relay_enabled, source: __MODULE__})
+    Bus.broadcast("messages", %Bus.Message{name: :main_positive_relay_enabled, value: state.main_positive_relay_enabled, source: __MODULE__})
+    Bus.broadcast("messages", %Bus.Message{name: :precharge_relay_enabled, value: state.precharge_relay_enabled, source: __MODULE__})
+    state
+  end
+
+  defp start_precharge(state) do
+    :ok = enable_relay(state, :main_negative_relay_pin)
+    :ok = enable_relay(state, :precharge_relay_pin)
+    %{state | status: :starting}
+  end
+
+  defp end_precharge(state) do
+    :ok = enable_relay(state, :main_positive_relay_pin)
+    precharge_ending_timestamp = System.system_time(:millisecond) + @relay_operating_delay
+    %{state | status: :precharge_complete, precharge_ending_timestamp: precharge_ending_timestamp}
+  end
+
+  defp finish_precharge(state) do
+    :ok = disable_relay(state, :precharge_relay_pin)
+    %{state | status: :started}
+  end
+
+  defp switch_off(state) do
+    :ok = disable_relay(state, :precharge_relay_pin)
+    :ok = disable_relay(state, :main_negative_relay_pin)
+    :ok = disable_relay(state, :main_positive_relay_pin)
+    %{state | status: :off}
   end
 
   defp enable_relay(state, relay) do
