3c8ae42704a1037cfa59ff57fc768ed534ef9267
Marc Lainez <marc.lainez@gmail.com>
2024-10-14 14:53:04 +0200

    Add orchestrator capabilities to rclex_teleop

--- rosbridge/rclex_teleop/lib/rclex_teleop/application.ex
+++ rosbridge/rclex_teleop/lib/rclex_teleop/application.ex
@@ -7,8 +7,17 @@ defmodule RclexTeleop.Application do
 
   @impl true
   def start(_type, _args) do
+    orchestrator = Application.get_env(:rclex_teleop, :orchestrator)
     children = [
-      {RclexTeleop.Teleop, []}
+      %{
+        id: RclexTeleop.Teleop,
+        start: {
+          RclexTeleop.Teleop,
+          :start_link, [%{
+            orchestrator: orchestrator
+          }]
+        }
+      }
     ]
     opts = [strategy: :one_for_one, name: RclexTeleop.Supervisor]
     Supervisor.start_link(children, opts)
