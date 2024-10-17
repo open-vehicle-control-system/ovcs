938c505d2902563ef01e7cc5de56ba51e87854cb
Marc Lainez <marc.lainez@gmail.com>
2024-10-17 11:15:10 +0200

    Add camera_info config

--- rosbridge/firmware/config/config.exs
+++ rosbridge/firmware/config/config.exs
@@ -13,6 +13,7 @@ config :rclex,
     "std_msgs/msg/Header",
     "sensor_msgs/msg/Image",
     "sensor_msgs/msg/CompressedImage",
+    "sensor_msgs/msg/CameraInfo",
     "geometry_msgs/msg/Twist",
   ]
 
@@ -23,7 +24,14 @@ config :rclex_cam,
       device: 0,
       topic: "front_left_camera",
       frame_id: "camera1",
-      props: %{width: 640, height: 480, fps: 30}
+      props: %{width: 640, height: 480, fps: 30},
+      info: %{
+        camera_matrix: [438.783367, 0.000000, 305.593336, 0.000000, 437.302876, 243.738352, 0.000000, 0.000000, 1.000000],
+        distortion_model: "plumb_bob",
+        distortion_coefficients: [-0.361976, 0.110510, 0.001014, 0.000505, 0.000000],
+        rectification_matrix: [0.999978, 0.002789, -0.006046, -0.002816, 0.999986, -0.004401, 0.006034, 0.004417, 0.999972],
+        projection_matrix: [393.653800, 0.000000, 322.797939, 0.000000, 0.000000, 393.653800, 241.090902, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000]
+      }
     },
     #%{
     #  process_name: FrontRightCamera,
