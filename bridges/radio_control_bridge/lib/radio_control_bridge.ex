a36d2d851cb09be4f2bf1afd48f2770c3086ce0e
Loic Vigneron <loic.vigneron@gmail.com>
2024-10-25 13:26:36 +0200

    Bootstrap radio control bridge

---
+++ bridges/radio_control_bridge/lib/radio_control_bridge.ex
@@ -0,0 +1,18 @@
+defmodule RadioControlBridge do
+  @moduledoc """
+  Documentation for `RadioControlBridge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RadioControlBridge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ bridges/radio_control_bridge/test/ovcs_radio_control_bridge_test.exs
@@ -0,0 +1,8 @@
+defmodule RadioControlBridgeTest do
+  use ExUnit.Case
+  doctest RadioControlBridge
+
+  test "greets the world" do
+    assert RadioControlBridge.hello() == :world
+  end
+end
