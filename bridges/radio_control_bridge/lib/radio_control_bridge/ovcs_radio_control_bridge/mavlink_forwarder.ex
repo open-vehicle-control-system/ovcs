e738e86aa539afe551fd8fcfef7ade01193aae55
Loic Vigneron <loic.vigneron@gmail.com>
2024-10-28 11:04:46 +0100

    Add cantastic frames for radio control bridge

--- bridges/radio_control_bridge/lib/radio_control_bridge/ovcs_radio_control_bridge/mavlink_forwarder.ex
+++ bridges/radio_control_bridge/lib/radio_control_bridge/ovcs_radio_control_bridge/mavlink_forwarder.ex
@@ -4,12 +4,33 @@ end
 
 defmodule RadioControlBridge.MavlinkForwarder do
   alias RadioControlBridge.MavlinkForwarder.State
+  alias Cantastic.Emitter
 
   require Logger
   use GenServer
 
   def init(_) do
     :ok = ExpressLrs.Mavlink.Interpreter.register_listener(self())
+    :ok = Emitter.configure(:ovcs, "radio_control_channels0", %{
+      parameters_builder_function: :default,
+      initial_data: %{
+        "channel1" => 0,
+        "channel2" => 0,
+        "channel3" => 0,
+        "channel4" => 0,
+      },
+      enable: true
+    })
+    :ok = Emitter.configure(:ovcs, "radio_control_channels1", %{
+      parameters_builder_function: :default,
+      initial_data: %{
+        "channel5" => 0,
+        "channel6" => 0,
+        "channel7" => 0,
+        "channel8" => 0,
+      },
+      enable: true
+    })
     {:ok, %State{}}
   end
 
@@ -22,7 +43,25 @@ defmodule RadioControlBridge.MavlinkForwarder do
   def handle_cast({:mavlink_message, mavlink_message}, state) do
     case mavlink_message.name do
       "RC_CHANNELS_OVERRIDE" ->
-        mavlink_message.base_fields |> Enum.reduce("", fn field, acc -> acc <> " #{field.value}" end) |> Logger.debug
+        channel_values = mavlink_message.base_fields |> Enum.reduce(%{}, fn (field, channel_values) ->
+          channel_values |> Map.put(field.name, field.value)
+        end)
+        :ok = Emitter.update(:ovcs, "radio_control_channels0", fn (data) ->
+          %{data |
+            "channel1" => channel_values["chan1_raw"],
+            "channel2" => channel_values["chan2_raw"],
+            "channel3" => channel_values["chan3_raw"],
+            "channel4" => channel_values["chan4_raw"],
+          }
+        end)
+        :ok = Emitter.update(:ovcs, "radio_control_channels1", fn (data) ->
+          %{data |
+            "channel5" => channel_values["chan5_raw"],
+            "channel6" => channel_values["chan6_raw"],
+            "channel7" => channel_values["chan7_raw"],
+            "channel8" => channel_values["chan8_raw"],
+          }
+        end)
       _ -> mavlink_message
     end
     {:noreply, state}
