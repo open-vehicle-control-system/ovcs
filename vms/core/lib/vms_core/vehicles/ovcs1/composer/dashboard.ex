7c9cbc28ee7f70f8da14ae825e7cbdb1d098c0ed
Marc Lainez <marc.lainez@gmail.com>
2024-11-22 12:35:21 +0100

    Add indication about which colors are supported

--- vms/core/lib/vms_core/vehicles/ovcs1/composer/dashboard.ex
+++ vms/core/lib/vms_core/vehicles/ovcs1/composer/dashboard.ex
@@ -5,7 +5,7 @@ defmodule VmsCore.Vehicles.OVCS1.Composer.Dashboard do
     %{
       vehicle: %{
         name: "OVCS1",
-        main_color: "indigo",
+        main_color: "indigo", # One of orange|red|blue|indigo|gray|green|amber|rose|teal
         refresh_interval: 70,
         pages: %{
           "dashboard"       => Dashboard.DashboardPage.definition(),
