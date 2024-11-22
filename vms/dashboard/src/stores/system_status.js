1d17f1cd02c2f0c1c237ae86c279ff08213ffd8c
Marc Lainez <marc.lainez@gmail.com>
2024-11-22 11:00:05 +0100

    Remove uneeded services and stores

--- vms/dashboard/src/stores/system_status.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useSystemStatus = defineStore('systemStatus', {
-    state: () => ({
-        failedEmitters: []
-    }),
-})
--- vms/dashboard/src/stores/vehicle.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useVehicle = defineStore('vehicle', {
-    state: () => ({
-        selectedGear: "parking",
-        speed: 0,
-        keyStatus: "off"
-    }),
-})
