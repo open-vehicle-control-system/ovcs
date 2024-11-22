1c70508bb4ca81eb931308bd683ada1e3030d527
Marc Lainez <marc.lainez@gmail.com>
2024-11-22 10:16:40 +0100

    Fix issue in terminate

--- vms/api/lib/vms_api_web/channels/metrics_channel.ex
+++ vms/api/lib/vms_api_web/channels/metrics_channel.ex
@@ -42,7 +42,7 @@ defmodule VmsApiWeb.MetricsChannel do
   end
 
   @impl true
-  def terminate(_, %{assigns: %{timer: timer}}) do
-    {:ok, _} = :timer.cancel(timer)
+  def terminate(_, %Phoenix.Socket{assigns: {_status, params}}) do
+    {:ok, _} = :timer.cancel(params.timer)
   end
 end
