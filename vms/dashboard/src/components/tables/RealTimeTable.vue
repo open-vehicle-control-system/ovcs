06d562579abfd5fb44e6941e3acfaa33f5f31305
Marc Lainez <marc.lainez@gmail.com>
2024-11-29 17:31:42 +0100

    Use heroicons instead of emojis for boolean values

--- vms/dashboard/src/components/tables/RealTimeTable.vue
+++ vms/dashboard/src/components/tables/RealTimeTable.vue
@@ -4,48 +4,54 @@
         <tbody class="divide-y divide-gray-200 bg-white">
             <tr v-for="metric in metrics">
                 <td class="text-wrap whitespace-nowrap px-3 py-4 text-sm font-bold text-gray-600">{{ metric.name }}</td>
-                <td v-if="store.data[metric.module]"  class="text-gray-500 text-wrap break-all whitespace-nowrap px-3 py-4 text-sm text-right">{{ displayValue(store, metric) }}</td>
+                <td v-if="store.data[metric.module]" class="text-gray-500 text-wrap break-all whitespace-nowrap px-3 py-4 text-sm text-right ">
+                    <component v-if="valueType(store, metric) === 'check'" :is="CheckIcon" class="h-6 w-6 inline-flex"></component>
+                    <component v-if="valueType(store, metric) === 'xmark'" :is="XMarkIcon" class="h-6 w-6 inline-flex"></component>
+                    <span v-if="valueType(store, metric) === 'none'">
+                        {{ renderValue(store, metric) }}
+                    </span>
+
+                </td>
             </tr>
         </tbody>
     </table>
 </template>
 
 <script setup>
-import data from 'emoji-mart-vue-fast/data/all.json'
-import { EmojiIndex } from 'emoji-mart-vue-fast/src'
-let emojiIndex = new EmojiIndex(data)
-
 const props = defineProps(['title', 'metrics', 'interval', 'store'])
+
+import { CheckIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const title = props.title
 let metrics = props.metrics
 let store = props.store
 
-const displayValue = (store, metric) => {
+const valueType = (store, metric) => {
     let value = store.data[metric.module][metric.key]
     if(typeof(value) === "boolean"){
         return renderBoolean(value);
-    } else if(typeof(value) === "number"){
-        return renderNumber(value, metric);
-    } else {
-        let displayValue = store.data[metric.module][metric.key]
-        metric.unit != null? displayValue = displayValue + " " + metric.unit : undefined
-        return displayValue
+    } else{
+        return "none"
     }
 }
 
 const renderBoolean = (value) => {
     if(value === true){
-        return emojiIndex.findEmoji(":white_check_mark:").native
+        return "check"
     } else if(value === false) {
-        return emojiIndex.findEmoji(":x:").native
+        return "xmark"
     }
 }
 
-const renderNumber = (value, metric) => {
-    let displayValue = Math.round(value*100)/100
-    metric.unit != null? displayValue = displayValue + " " + metric.unit : undefined
-    return displayValue
+const renderValue = (store, metric) => {
+    let value = store.data[metric.module][metric.key]
+    if(typeof(value) === "number"){
+        let displayValue = Math.round(value*100)/100
+        metric.unit != null? displayValue = displayValue + " " + metric.unit : undefined
+        return displayValue
+    } else {
+        return value
+    }
 }
 
 </script>
