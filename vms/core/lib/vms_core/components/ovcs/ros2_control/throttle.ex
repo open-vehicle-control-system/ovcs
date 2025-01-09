5b28a65ba3ed24468c3146893743bf6e83bdab7f
Marc Lainez <marc.lainez@gmail.com>
2025-01-09 11:10:58 +0100

    Add throttle calculation based on rosbridge information

--- vms/core/lib/vms_core/components/ovcs/ros2_control/throttle.ex
+++ vms/core/lib/vms_core/components/ovcs/ros2_control/throttle.ex
@@ -5,12 +5,15 @@ defmodule VmsCore.Components.OVCS.Ros2Control.Throttle do
   use GenServer
   alias Cantastic.{Receiver, Frame}
   alias Decimal, as: D
-  alias VmsCore.Bus
+  alias VmsCore.{Bus, PID}
 
   @loop_period 10
   @zero D.new(0)
-  @min_value 0 # in m/s
-  @max_value 4 # in m/s
+  @min_value 0 # in km/h
+  @max_value 15 # in km/h
+  @kp D.new("0.2")
+  @ki D.new("0.1")
+  @kd D.new("0.1")
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
@@ -19,16 +22,27 @@ defmodule VmsCore.Components.OVCS.Ros2Control.Throttle do
   @impl true
   def init(_args) do
     channel_frame_name = "ros2_control"
+    speed_frame_name = "abs_status"
     :ok = Receiver.subscribe(self(), :ovcs, channel_frame_name)
+    :ok = Receiver.subscribe(self(), :polo_drive, speed_frame_name)
     {:ok, timer} = :timer.send_interval(@loop_period, :loop)
-    {:ok, %{
+    state = %{
       loop_timer: timer,
       frame_name: channel_frame_name,
+      speed_frame_name: speed_frame_name,
       name: "linear",
+      speed_name: "speed",
       desired_speed: @zero,
+      actual_speed: @zero,
       requested_throttle: @zero,
-      requested_gear: :drive
-    }}
+      requested_gear: :parking,
+      pid: nil,
+      kp: @kp,
+      ki: @ki,
+      kd: @kd
+    }
+    pid = init_pid(state)
+    {:ok, %{state | pid: pid}}
   end
 
   @impl true
@@ -45,15 +59,30 @@ defmodule VmsCore.Components.OVCS.Ros2Control.Throttle do
     {:noreply, %{state | desired_speed: desired_speed}}
   end
 
+  def handle_info({:handle_frame, %Frame{name: name, signals: signals}}, state) when name == state.speed_frame_name do
+    actual_speed = signals[state.speed_name].value
+    {:noreply, %{state | actual_speed: actual_speed}}
+  end
+
   defp compute_throttle(state) do
-    # TODO use a PID for speed
-    requested_throttle = 0
-    %{state | requested_throttle: requested_throttle}
+    pid = PID.iterate(state.pid, state.actual_speed, state.desired_speed)
+    requested_throttle = pid.output
+    requested_gear = if requested_throttle >= 0, do: :drive, else: :reverse
+    %{state | requested_throttle: requested_throttle, requested_gear: requested_gear, pid: pid}
   end
 
   defp emit(state) do
     Bus.broadcast("messages", %Bus.Message{name: :requested_gear, value: state.requested_gear, source: __MODULE__})
     Bus.broadcast("messages", %Bus.Message{name: :requested_throttle, value: state.requested_throttle, source: __MODULE__})
     state
+  end
+
+  defp init_pid(state) do
+    PID.new(
+      kp: state.kp,
+      ki: state.ki,
+      kd: state.kd,
+      reset_derivative_when_setpoint_changes: true
+    )
   end
 end
