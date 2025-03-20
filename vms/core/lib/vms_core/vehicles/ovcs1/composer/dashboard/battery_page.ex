e85ac000e501c6e12a9da8d87e4d0e4d83092bf2
Thibault Poncelet <thibault@spin42.com>
2025-03-20 10:49:02 +0100

    Display charger status

--- vms/core/lib/vms_core/vehicles/ovcs1/composer/dashboard/battery_page.ex
+++ vms/core/lib/vms_core/vehicles/ovcs1/composer/dashboard/battery_page.ex
@@ -1,5 +1,5 @@
 defmodule VmsCore.Vehicles.OVCS1.Composer.Dashboard.BatteryPage do
-  alias VmsCore.Components.Nissan.LeafZE0.Charger
+  alias VmsCore.Components.Evpt.Evpt23Charger
 
   def definition(order: order) do
     %{
@@ -12,10 +12,13 @@ defmodule VmsCore.Vehicles.OVCS1.Composer.Dashboard.BatteryPage do
           name: "Charger Status",
           type: "table",
           rows: [
-            %{type: :metric, name: "Charging State", module: Charger, key: :charging_state},
-            %{type: :metric, name: "AC Voltage", module: Charger, key: :ac_voltage, unit: "V"},
-            %{type: :metric, name: "Charge Power", module: Charger, key: :charge_power, unit: "kW"},
-            %{type: :metric, name: "Maximum Charge Power", module: Charger, key: :maximum_charge_power, unit: "kW"}
+            %{type: :metric, name: "Output voltage", module: Evpt23Charger, key: :output_voltage, unit: "V"},
+            %{type: :metric, name: "Output current", module: Evpt23Charger, key: :output_current, unit: "A"},
+            %{type: :metric, name: "CAN failure", module: Evpt23Charger, key: :communication_timeout_failure},
+            %{type: :metric, name: "Battery disconnected or reversed", module: Evpt23Charger, key: :communication_timeout_failure},
+            %{type: :metric, name: "AC voltage failure", module: Evpt23Charger, key: :battery_disconnected_or_reverse_connection_protection_enabled},
+            %{type: :metric, name: "Charger over temp. failure", module: Evpt23Charger, key: :charger_over_temperature_protection_enabled},
+            %{type: :metric, name: "Hardware failure", module: Evpt23Charger, key: :hardware_failure}
           ]
         },
       }
