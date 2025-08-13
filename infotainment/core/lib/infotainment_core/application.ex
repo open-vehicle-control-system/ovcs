a2866e2a46297bf1960f6e195c58ef82a5d8aff6
Marc Lainez <marc.lainez@gmail.com>
2025-08-13 16:50:39 +0200

    Make infotainment code multi vehicle

--- infotainment/core/lib/infotainment_core/application.ex
+++ infotainment/core/lib/infotainment_core/application.ex
@@ -7,17 +7,24 @@ defmodule InfotainmentCore.Application do
 
   @impl true
   def start(_type, _args) do
+    vehicle_children = vehicle()
     children = [
       #InfotainmentCore.Repo,
       #{Ecto.Migrator,
       #  repos: Application.fetch_env!(:vms_core, :ecto_repos),
       #  skip: skip_migrations?()},
-      {InfotainmentCore.VehicleStatus, []},
+      {vehicle_children, []},
       {InfotainmentCore.Temperature, []}
-
     ]
+
+    children
 
     opts = [strategy: :one_for_one, name: InfotainmentCore.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def vehicle do
+    InfotainmentCore.Vehicles
+      |> Module.concat(Application.get_env(:infotainment_core, :vehicle))
   end
 end
---
+++ infotainment/core/lib/infotainment_core/vehicles/obd2.ex
@@ -0,0 +1,56 @@
+defmodule InfotainmentCore.Vehicles.OBD2 do
+  @moduledoc """
+    Implements a basic OBD2 mapper reading RPM and speed
+  """
+  use GenServer
+  require Logger
+  alias Decimal, as: D
+  alias Cantastic.{Signal, Receiver, Emitter, Frame}
+
+  @zero D.new(0)
+  @loop_period 20
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_) do
+    :ok = Receiver.subscribe(self(), :ovcs, ["obd2_status"])
+    {:ok, timer} = :timer.send_interval(@loop_period, :loop)
+    {:ok, %{
+      loop_timer: timer,
+      rotation_per_minute: @zero,
+      speed: @zero,
+    }}
+  end
+
+  @impl true
+  def handle_info(:loop, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:handle_frame, %Frame{name: "obd2_status", signals: signals}}, state) do
+    %{"speed" => %Signal{value: speed}} = signals
+    %{"rotation_per_minute" => %Signal{value: rotation_per_minute}} = signals
+
+    {:noreply, %{
+      state |
+        speed: D.new(speed),
+        rotation_per_minute: D.new(rotation_per_minute)
+      }
+    }
+  end
+
+  def handle_call(:status, _from, state) do
+    status = state |> Map.take([
+      :speed,
+      :rotation_per_minute,
+    ])
+    {:reply, {:ok, status}, state}
+  end
+
+  def status() do
+    GenServer.call(__MODULE__, :status)
+  end
+end
